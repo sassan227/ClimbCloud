06f5c6205d8e8b253a8557629d4eda9ed010aa2b
sassan0702 <sassan0702@outlook.jp>
2021-08-09 23:17:46 +0900

    完成

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //LoadSceneを使うために必要！！
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid2d;
+    Animator animator;
+    float jumpForce = 680.0f;
+    float walkForce = 90.0f;
+    float maxWalkSpeed = 2.0f;
+    float threshold = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid2d = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ジャンプする
+        if (Input.GetMouseButtonDown(0) &&
+            this.rigid2d.velocity.y == 0)
+        {
+            this.animator.SetTrigger("JumpTrigger");
+            this.rigid2d.AddForce(transform.up * this.jumpForce);
+        }
+
+        //左右移動
+        int key = 0;
+        if (Input.acceleration.x > this.threshold) key = 1;
+        if (Input.acceleration.x < -this.threshold) key = -1;
+
+        //プレイヤの速度
+        float speedx = Mathf.Abs(this.rigid2d.velocity.x);
+
+        //スピード制限
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2d.AddForce(transform.right * this.walkForce * key);
+        }
+
+        // 動く方向に応じて反転
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        //プレイヤの速度に応じてアニメーション速度を変える
+        if(this.rigid2d.velocity.y == 0)
+        {
+            this.animator.speed = speedx / 2.0f;
+        }
+        else
+        {
+            this.animator.speed = 1.0f;
+        }
+
+        // 画面外に出た場合は最初から
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+
+
+    // ゴールに到達
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("ゴール");
+        SceneManager.LoadScene("ClearScene");
+    }
+}
